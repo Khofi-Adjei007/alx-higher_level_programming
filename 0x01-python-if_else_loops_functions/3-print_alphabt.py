9a6e1e9cc3cbedad5a8d667399e87c0e941f3199
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-06-06 13:50:10 -0700

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print("".join(["{}".format(chr(x)) for x in range(ord('a'), ord('z')+1) if chr(x) not in ('q', 'e')]), end='')
+print("".join(["{}".format(chr(x)) for x in range(
+    ord('a'), ord('z')+1) if chr(x) not in ('q', 'e')]), end='')
+
