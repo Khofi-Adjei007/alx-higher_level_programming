8856bda23a9e0619ad34570ae797af258df8eff0
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-06-06 15:26:59 -0700

    4-print_hexa.py

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-print("".join([str(num) + " " + hex(num) + "\n" if (num + 1) %
-      15 != 0 else str(num) + " " + hex(num) for num in range(99)]))
+print("".join(["{} {}\n".format(num, hex(num)) if (num + 1) % 15 !=
+      0 else "{} {}\n".format(num, hex(num))[:-1] for num in range(99)]))
