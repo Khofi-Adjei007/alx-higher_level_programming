78681e9b82a92c316fdb63ef3eb9ed4c2ca59ee1
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-06-07 06:37:18 -0700

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('A') - 1, -1):
-    letter = chr(i)
-    case = 'lowercase' if i % 2 == 0 else 'uppercase'
-    print("{:s}".format(letter.lower() if case ==
-          'lowercase' else letter.upper()), end="")
 
-print()
-
+for i in range(90, 64, -1):
+    print(chr(i), end='')
+    print(chr(i + 32), end='')
