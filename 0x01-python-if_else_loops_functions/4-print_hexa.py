256542f40bcfb96de220778687a9bc67408744dc
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-06-07 03:08:55 -0700

    4-print_hexa.py

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-print("".join(["{} {}\n".format(num, hex(num)) if (num + 1) % 15 !=
-      0 else "{} {}\n".format(num, hex(num))[:-1] for num in range(99)]))
+for i in range(99):
+    print("{} - {}".format(i, hex(i)))
