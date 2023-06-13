165cc2202d228cbbb2cf75eb06211756c6fe139e
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-06-13 13:47:05 -0700

    13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,41 +2,58 @@
 #include <stdlib.h>
 #include "lists.h"
 
-int len_list(listint_t *head)
-{
-	int i = 0;
-	listint_t *h = head;
-
-	while (head != NULL)
-	{
-		h = h->next;
-		i++;
-	}
-	return (i);
-}
-
 /**
  * is_palindrome - checks if a singly linked list is a palindrome.
- * @head: singly linked list.
+ * @head: double pointer to the head of the linked list.
  * Return: 0 if it is not a palindrome, 1 if it is a palindrome.
  */
 int is_palindrome(listint_t **head)
 {
-	int a, i;
-	listint_t *tmp = *head;
-	int array;
+	listint_t *slow = *head;
+	listint_t *fast = *head;
+	listint_t *prev = NULL;
+	listint_t *temp = NULL;
+	int palindrome = 1;
 
-	l = len_list(*head);
-	array = malloc(sizeof(int) * l);
+	if (*head == NULL || (*head)->next == NULL)
+		return 1;
 
-	if (array == NULL)
-	        exit(-1);
+	/* Find the middle element of the linked list */
+	while (fast != NULL && fast->next != NULL)
+	{
+		fast = fast->next->next;
+		temp = slow;
+		slow = slow->next;
+		temp->next = prev;
+		prev = temp;
+	}
 
-	i = 0;
+	/* Adjust pointers for odd or even number of nodes */
+	if (fast != NULL)
+		slow = slow->next;
+
+	/* Compare the first half with the reversed second half */
+	while (slow != NULL)
+	{
+		if (slow->n != prev->n)
+		{
+			palindrome = 0;
+			break;
+		}
+		slow = slow->next;
+		prev = prev->next;
+	}
+
+	/* Restore the original structure of the linked list */
+	fast = temp;
 	while (temp != NULL)
 	{
-		array[i] = temp->n;
-		temp = temp->next;
-		i++;
-	}	
+		prev = temp->next;
+		temp->next = fast;
+		fast = temp;
+		temp = prev;
+	}
+
+	*head = fast;
+	return palindrome;
 }
