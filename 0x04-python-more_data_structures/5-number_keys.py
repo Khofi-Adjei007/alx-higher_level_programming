6787981b6c056ee3008844563696af613b3a416b
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-06-14 14:06:03 -0700

    5-number_keys.py

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    # Get the keys of the dictionary using the keys() method
+    keys = a_dictionary.keys()
+
+    # Count the number of keys using the len() function
+    num_keys = len(keys)
+
+    return num_keys
