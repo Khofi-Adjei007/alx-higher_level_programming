feecd3aaf006c95f0e889e231d97656ddfc9fc9a
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-06-14 16:47:50 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
+# add_0.py
 def add(a, b):
-    return a 
+    return a + b
 
-import add_0
-
-a = 1
-b = 2
-
-result = add_0.add(a, b)
-print("{} + {} = {}".format(a, b, result))
+if __name__ == '__main__':
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
