0386d765c15fd3388ef9d71c76a762a057be3e9a
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-06-14 14:28:50 -0700

    7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    updated_dictionary = dict(a_dictionary)
-    updated_dictionary[key] = value
-    return updated_dictionary
+    a_dictionary[key] = value
+    return a_dictionary
