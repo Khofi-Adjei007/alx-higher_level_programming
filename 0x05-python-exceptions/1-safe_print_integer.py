ad7aba400fa9675463479e79a395fc1d893ba392
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-06-26 15:22:10 -0700

    1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -7,20 +7,25 @@ def safe_print_integer(value):
         value: The value to print.
 
     Returns:
-        bool: True if value has been correctly printed as an integer, False otherwise.
+        bool: True if value has been correctly
+        printed as an integer, False otherwise.
 
     Description:
-        This function takes a value as input and attempts to format it as an integer using the "{:d}".format() syntax.
-        If the formatting is successful, the integer value is printed followed by a new line, and True is returned.
+        This function takes a value as input and attempts to
+        format it as an integer using the "{:d}".format() syntax.
+        If the formatting is successful, the integer value is printed
+        followed by a new line, and True is returned.
 
-        If the value cannot be formatted as an integer due to a ValueError or TypeError, False is returned.
+        If the value cannot be formatted as an integer due
+        to a ValueError or TypeError, False is returned.
 
-        The function uses a try-except block to catch any exceptions that may occur during the formatting process.
+        The function uses a try-except block to catch any exceptions
+        that may occur during the formatting process.
     """
+
     try:
         formatted_value = "{:d}".format(value)
         print(formatted_value)
         return True
     except (ValueError, TypeError):
         return False
-
