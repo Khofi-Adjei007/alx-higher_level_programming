4a2cd37cd8e6b82fd7f49cac7d34d3c4c70d0bed
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-06-26 17:34:14 -0700

    3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -8,21 +8,27 @@ def safe_print_division(a, b):
         b (int): The denominator.
 
     Returns:
-        float or None: The result of the division, or None if division by zero occurs.
+        float or None: The result of the division,
+        or None if division by zero occurs.
 
     Description:
-        This function takes two integers, a (numerator) and b (denominator), as input.
-        It performs the division operation a / b and handles potential exceptions.
+        This function takes two integers, a
+        (numerator) and b (denominator), as input.
+        It performs the division operation a / b
+        and handles potential exceptions.
 
-        If b is 0, a ZeroDivisionError occurs, and None is returned. Otherwise, the division is successful,
+        If b is 0, a ZeroDivisionError occurs, and
+        None is returned. Otherwise, the division is successful,
         and the result is stored in the 'result' variable.
 
-        The function then enters the 'finally' block, where it prints the result using "{}".format() to format it,
+        The function then enters the 'finally' block, where it prints the
+        result using "{}".format() to format it,
         preceded by the text 'Inside result:'.
 
         Finally, the function returns the result, which can be a float or None.
 
-        The use of try-except-finally allows for proper exception handling and ensures that the result is always printed.
+        The use of try-except-finally allows for proper exception
+        handling and ensures that the result is always printed.
     """
     result = None  # Initialize result to None
     try:
