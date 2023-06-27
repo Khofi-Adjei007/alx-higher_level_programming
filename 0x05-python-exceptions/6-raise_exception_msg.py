78116a56f21799a9696eaf00506fc745600c7feb
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-06-26 17:47:47 -0700

    6-raise_exception_msg.py

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,11 +1,27 @@
+#!/usr/bin/python3
 def raise_exception_msg(message=""):
     """
-    Raises a NameError exception with a custom message.
+    Raises a name exception with a message.
 
     Args:
-        message (str): The custom message to include in the exception.
+        message (str): The message to include in
+        the exception. Default is an empty string.
 
     Raises:
-        NameError: Always raises a NameError exception with the specified message.
+        NameError: Always raises a
+        NameError with the provided message.
+
+    Description:
+        This function raises a NameError
+        exception with the given message.
+        If no message is provided, an empty
+        string is used as the default.
     """
     raise NameError(message)
+
+
+if __name__ == "__main__":
+    try:
+        raise_exception_msg("C is fun")
+    except NameError as ne:
+        print(ne)
