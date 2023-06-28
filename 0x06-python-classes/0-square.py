78a49285f33efe7b7eb1b88e3a07dcec2b14b4dc
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-06-28 14:07:48 -0700

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,35 @@
 #!/usr/bin/python3
-
-""" This is an empty class """
-
-
 class Square:
-    pass
+    """
+    Represents a square.
+
+    Attributes:
+        None
+    """
+
+    def __init__(self):
+        """
+        Initializes a Square object.
+
+        Args:
+            None
+        """
+        pass
+
+    def calculate_area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+            float: The area of the square.
+        """
+        pass
+
+    def calculate_perimeter(self):
+        """
+        Calculates the perimeter of the square.
+
+        Returns:
+            float: The perimeter of the square.
+        """
+        pass
