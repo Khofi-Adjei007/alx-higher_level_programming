78a4898f7f240cdc871be75e7570068d8f9c3e5f
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-07-08 06:42:59 -0700

    0-square.py

--- 0x06-python-classes/0-main.py
+++ 0x06-python-classes/0-main.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""
+Square Module
+
+This module demonstrates the usage of the Square
+class defined in the "0-square" module.
+It creates a Square object, prints its type, and
+displays its dictionary representation.
+"""
+
 Square = __import__('0-square').Square
 
 my_square = Square()
