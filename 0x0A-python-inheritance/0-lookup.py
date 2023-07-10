b0690577f63ccfa91f22c9959afe0f81aa48baa0
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-07-10 06:54:31 -0700

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,16 +1,25 @@
 def lookup(obj):
     """
     Returns a list of available attributes and methods of an object.
+
     Args:
         obj: An object to inspect.
+
     Returns:
         A list of available attributes and methods of the object.
     """
-    # Use the dir() function to get a sorted list of names in the object
-    # This includes attributes, methods, and special methods
-    names = dir(obj)
-    # Filter out names starting with '__' (double underscores)
-    # as these are typically considered private or special methods
-    available_names = [name for name in names if not name.startswith('__')]
-    # Return the list of available attributes and methods
-    return available_names
+    # Use the dir() function to retrieve a list of all names associated with the object
+    all_names = dir(obj)
+
+    # Initialize an empty list to store the available attributes and methods
+    available_attributes_and_methods = []
+
+    # Iterate through each name in the list of all names
+    for name in all_names:
+        # Check if the name does not start with '__' (double underscores)
+        if not name.startswith('__'):
+            # Append the name to the list of available attributes and methods
+            available_attributes_and_methods.append(name)
+
+    # Return the final list of available attributes and methods
+    return available_attributes_and_methods
