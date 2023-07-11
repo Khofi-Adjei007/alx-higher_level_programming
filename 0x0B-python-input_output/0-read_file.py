7ebbf1d8571a8792acf3ef062fcf754ffbea9547
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-07-11 15:34:05 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
 
 def read_file(filename=""):
-    """function that reads a text file and prints it"""
-    with open(filename) as f:
-        text = f.read()
-        print(text, end="")
+
+    """
+    Reads a UTF-8 text file and prints its contents to stdout.
+    Args:
+        filename (str, optional): The name of the text file to be read.
+                                 Defaults to an empty string.
+    Raises:
+        FileNotFoundError: If the specified file does not exist,
+        this exception will not be raised
+        as per the requirements.
+    Returns:
+        None
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        # Read the contents of the file
+        contents = file.read()
+
+        # Print the contents to stdout
+        print(contents)
