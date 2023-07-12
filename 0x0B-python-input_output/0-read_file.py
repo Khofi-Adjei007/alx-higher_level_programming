c91c13a9a7cd7fbd056831890ef46e077c2ee76b
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-07-12 12:44:34 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
 
+    """
+    Reads a text file (UTF8) and prints its contents to stdout.
+
+    Args:
+        filename (str): The name of the file to read. Default is an empty string.
+
+    Returns:
+        None
+
+    Raises:
+        None
+    """
+
 def read_file(filename=""):
     with open(filename, 'r', encoding='utf-8') as file:
 
@@ -7,4 +20,4 @@ def read_file(filename=""):
         contents = file.read()
 
         # Print the contents to stdout
-        print(contents)
+       ` print(contents)
